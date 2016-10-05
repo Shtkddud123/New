6d3c5dc080143e3098ae8a0c7ccf0e92d4bc5668
Shtkddud123 <sangyoung123@googlemail.com>
2016-10-05 21:42:48 +0100

    Edited some files

--- C++_Stuff/Sally.cpp
+++ C++_Stuff/Sally.cpp
@@ -8,26 +8,37 @@
 using namespace std;
 
 Sally::Sally() {
+
 }
 
 Sally::Sally(int a) {
+
   num = a; 
 }
 
 Sally Sally::operator+(Sally aso) { // a sallyo object 
+
   Sally brandNew;
   // new sally object num is this->num + aso.nu,
+
   brandNew.num =  this->num + aso.num;
   return(brandNew); // returns the brandnew sally object 
 }
 
-mapexample::mapexample() {}
-mapexample::~mapexample() {}
+mapexample::mapexample() {} // Constructor 
+
+mapexample::~mapexample() {} // Destructor 
+
 void mapexample::addtomap(char a, int b) {
   mymap[a] = b;
 }
+
 void mapexample::printmap() {
+
   for (map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it) {
+
     cout << it->first << " => " << it->second << '\n';
+
   }
+
 }
